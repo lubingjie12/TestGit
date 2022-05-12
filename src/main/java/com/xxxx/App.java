630a31cda78cb86ad1335e9b2ecb0cd1041e0bcf
lbj <2434654643@qq.com>
2022-05-12 19:47:41 +0800

    忽略测试

--- src/main/java/com/xxxx/App.java
+++ src/main/java/com/xxxx/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("你好");
         System.out.println("dev");
+        System.out.println("忽略测试");
     }
 }
